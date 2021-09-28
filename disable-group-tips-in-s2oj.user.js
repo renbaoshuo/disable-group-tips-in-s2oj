c86f7eac8d8081798a2df9f8817d3492bf36b069
Ren Baoshuo <i@baoshuo.ren>
2021-09-28 18:35:29 +0800

    fix: adapt to S2OJ v2.0

--- disable-group-tips-in-s2oj.user.js
+++ disable-group-tips-in-s2oj.user.js
@@ -16,10 +16,10 @@
 
 (function() {
     'use strict';
-    document.querySelectorAll('body > div > div.card.card-default > div > p:nth-child(1)')[0].innerHTML += `<a href="javascript:;" id="closeGroupTips" style="float: right;"></a><style>.no-display { display: none; }</style>`;
+    document.querySelectorAll('#user-group > div > p:nth-child(1)')[0].innerHTML += `<a href="javascript:;" id="closeGroupTips" style="float: right;"></a><style>.no-display { display: none; }</style>`;
     if(typeof localStorage.displayGroupTips !== "undefined" && localStorage.displayGroupTips === "false") {
         document.getElementById('closeGroupTips').innerHTML = `[显示]`;
-        Array.from(document.querySelectorAll('body > div.container.theme-showcase > div.card.card-default > div > :not(:first-child)')).forEach(elem => {
+        Array.from(document.querySelectorAll('#user-group > div > :not(:first-child)')).forEach(elem => {
             elem.classList.add('no-display');
         });
         localStorage.displayGroupTips = "false";
@@ -31,7 +31,7 @@
     } else {
         localStorage.displayGroupTips = "true";
             document.getElementById('closeGroupTips').innerHTML = `[隐藏]`;
-            Array.from(document.querySelectorAll('body > div.container.theme-showcase > div.card.card-default > div > :not(:first-child)')).forEach(elem => {
+            Array.from(document.querySelectorAll('#user-group > div > :not(:first-child)')).forEach(elem => {
             elem.classList.remove('no-display');
         });
         localStorage.displayGroupTips = "true";
